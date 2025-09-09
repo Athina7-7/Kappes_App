a9046aedc24434679a29c89ac716b9a849dab9b1
Athina Cappelletti <84542209+Athina7-7@users.noreply.github.com>
2025-09-09 16:40:04 -0500

    Vinculación con los avances

--- principal/admin.py
+++ principal/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Mesa, Producto, tipoVenta, Usuario, Orden, detalleOrden, zonaDomicilio
 
 # Registramos cada modelo en el admin para poder visualziarlo en: http://127.0.0.1:8000/admin/
+
 admin.site.register(Mesa)
 admin.site.register(Producto)
 admin.site.register(tipoVenta)
