e89d755f0aca44282697c0504c9bf47472d9a2ac
mariamny <mariamnydelvalle@gmail.com>
2025-09-24 21:30:57 -0500

    Diseno de inicio-sesion

--- principal/views/views_inicio_sesion.py
+++ principal/views/views_inicio_sesion.py
@@ -8,15 +8,25 @@ def inicio_sesion(request):
         nombre = request.POST.get("Nombre")
         contrasena = request.POST.get("Contraseña")
 
+        #1) Verificar si uno o ambos campos están vacíos
+        if not nombre or not contrasena:
+            messages.warning(request, "Datos incompletos")
+            return render(request, "inicio_sesion.html")
+
+
+        #2) Se busca el usuario en la base de datos
         try:
             usuario = Usuario.objects.get(nombre=nombre)
-            # Comparamos usando check_password
+
+            # 3) Comparamos usando check_password
             if check_password(contrasena, usuario.contrasena):
                 # Si la contraseña es correcta
                 return redirect("home")  # o a donde quieras llevarlo
             else:
+                #Contraseña incorrecta
                 messages.error(request, "Usuario o contraseña incorrectos")
         except Usuario.DoesNotExist:
+            #Usuario no encontrado
             messages.error(request, "Usuario o contraseña incorrectos")
 
     return render(request, "inicio_sesion.html")
