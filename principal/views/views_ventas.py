03b75bdb48b57105078c9490cf663fdfdb57f368
Athina7-7 <cappellettiathina@gmail.com>
2025-11-10 22:54:52 -0500

    Merge con ventas

--- principal/views/views_ventas.py
+++ principal/views/views_ventas.py
@@ -3,11 +3,20 @@ from principal.models import Orden
 
 def mostrar_ventas(request):
     query = request.GET.get('q', '')
-    if query:
-        ordenes = Orden.objects.filter(numero_orden__icontains=query, oculta=False).order_by('-fecha')
-    else:
-        ordenes = Orden.objects.filter(oculta=False).order_by('-fecha')
     
+    # Filtrar solo órdenes pagadas y no ocultas
+    if query:
+        ordenes = Orden.objects.filter(
+            numero_orden__icontains=query,
+            oculta=False,
+            estado_pago='pago'
+        ).order_by('-fecha')
+    else:
+        ordenes = Orden.objects.filter(
+            oculta=False,
+            estado_pago='pago'
+        ).order_by('-fecha')
+
     return render(request, 'ventas.html', {
         'ordenes': ordenes,
         'query': query
